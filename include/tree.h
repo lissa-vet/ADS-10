ba9e1b2c5e4cc37824512d52372a3102e064568f
lissa-vet <125082940+lissa-vet@users.noreply.github.com>
2023-06-04 02:58:23 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,37 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
 
-#endif  // INCLUDE_TREE_H_
+class Tree {
+ private:
+  std::vector<Tree*> child;
+  char symb;
+  
+  explicit Tree(char temp) : symb(temp) {}
+  void addNode(std::vector<char> arr) {
+    for (int i = 0; i < arr.size(); i++) {
+      std::vector<char> temp = arr;
+      child.push_back(new Tree(temp[i]));
+      temp.erase(temp.begin() + i);
+      child[i]->addNode(temp);
+    }
+  }
+  
+ public:
+  explicit Tree(std::vector<char> vect) {
+    symb = '*';
+    addNode(vect);
+  }
+  int get_size() const {
+    return child.size();
+  }
+  char get_symb() const {
+    return symb;
+  }
+  Tree& operator[](int n) const {
+    return *child[n];
+  }
+};
+
+  #endif  // INCLUDE_TREE_H_
