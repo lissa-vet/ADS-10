a301d6b5b4f8242c0b5403bfd1126f508cae15fc
lissa-vet <125082940+lissa-vet@users.noreply.github.com>
2023-06-04 03:10:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,22 @@
 #include  <cstdlib>
 #include  "tree.h"
 
+int factor(int n) {
+  if (n <= 1) return 1;
+  return factor(n - 1) * n;
+}
+
 std::vector<char> getPerm(const Tree& tree, int n) {
   // напишите реализацию
+  if (!tree.get_size() || n > factor(tree.get_size()))
+    return {};
+  if (tree.get_symb() == '*')
+    n--;
+  int tmp = factor(tree.get_size() - 1);
+  std::vector<char> res;
+  res.push_back(tree[n / tmp].get_symb());
+  std::vector<char> a = getPerm(tree[n / tmp], n % tmp);
+  res.insert(res.end(), a.begin(), a.end());
+  
+  return res;
 }
